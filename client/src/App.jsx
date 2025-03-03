9f7df09e169ec68191100da6618572758dd85bc6
Kers1on <maximpro63@gmail.com>
2025-03-03 21:42:21 +0100

    feat: add Routers and files for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Students from './pages/students'
+import Teachers from './pages/teachers'
+import Faculty from './pages/faculty'
 
 function App() {
   return (
-    <button type="button" class="btn-primary">Base class</button>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/students' element={<Students />} />
+        <Route path='/teachers' element={<Teachers />} />
+        <Route path='/faculty' element={<Faculty />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
