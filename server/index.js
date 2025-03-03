0719e3a87f3cb8ebe71ff2f41075034e6419aa4c
Kers1on <maximpro63@gmail.com>
2025-03-03 20:52:42 +0100

    feat: add from server print page

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
-import express from "express";
-import { db } from "./db";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 8747;
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+})
 
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
